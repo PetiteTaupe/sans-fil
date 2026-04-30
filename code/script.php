600e909ecc5ccf30d7eadc72e164f7e5443031f1
Béchir Alexis <alexis.bechir@he-arc.ch>
2026-04-30 10:21:28 +0200

    webhook

--- code/script.php
+++ code/script.php
@@ -1,3 +1,37 @@
 <?php
 $stderr = fopen('php://stderr', 'w');
-fwrite($stderr, file_get_contents('php://input') . "\n");
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $raw = file_get_contents('php://input');
+    $data = json_decode($raw, true);
+
+    $decoded = $data['uplink_message']['decoded_payload']['data'];
+
+    fwrite($stderr, "Decoded payload: " . print_r($decoded, true) . "\n");
+
+    // stocker
+    file_put_contents('data.json', json_encode($decoded));
+
+    exit;
+}
+
+$data = json_decode(@file_get_contents('data.json'), true);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Labo sans fil</title>
+    <meta http-equiv="refresh" content="10">
+</head>
+<body>
+    <h1>Données capteurs</h1>
+
+    <p>Température: <?= $data['temp1']?> °C</p>
+    <p>Humidité: <?= $data['humidity']?> %</p>
+    <p>Batterie: <?= $data['battery_voltage']?> mV</p>
+
+</body>
+</html>
